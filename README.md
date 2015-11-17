2d52b998d805bee00c33dcbc6f28afa756877d26
Teyras <Teyras@gmail.com>
2015-11-17 17:46:04 +0100

    Added the worker script (proof of concept)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # BasicWorker
+
+A daemon that consumes assignments from the message queue, builds and runs them 
+and then sends results back.
+
+## How to run it
+
+- Install and start RabbitMQ (the daemon is currently hardwired to localhost)
+- Install Python 3 and the pika library
+- Run `main.py`
---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import pika
+import sys
+import os
+import time
+
+def callback(ch, method, properties, body):
+    print(" [x] Received {}".format(body))
+    time.sleep(10)
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host = "localhost"
+))
+channel = connection.channel()
+
+channel.exchange_declare(
+    exchange = "tasks", 
+    type = "direct", 
+    auto_delete = False
+)
+
+for env in sys.argv[1:]:
+    channel.queue_declare(
+        queue = env,
+        durable = True,
+        auto_delete = False
+    )
+    channel.queue_bind(
+        exchange = "tasks", 
+        queue = env
+    )
+    channel.basic_consume(callback, queue = env)
+
+print(" [*] Waiting for messages. Press CTRL+C to exit.")
+
+channel.basic_qos(prefetch_count = 1)
+
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    print("Interrupted")
