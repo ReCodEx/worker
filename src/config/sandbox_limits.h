cdf9c5951a3aac7ac2e67e91dfd43c2c175f977f
Martin Polanka <PolankaMartin@gmail.com>
2016-01-06 23:34:57 +0100

    All warnings enabled on linux machines. Sandbox limits docs.

--- src/config/sandbox_limits.h
+++ src/config/sandbox_limits.h
@@ -5,43 +5,104 @@
 
 
 /**
- *
- */
+* Sandbox limits.
+* @note Not all sandbox implementations must follow all these parameters.
+*/
 struct sandbox_limits {
 public:
-	size_t memory_usage = 0;	//limit memory usage by whole control group (kB)
-	float cpu_time = 0;			//limit total run time of whole control group (s)
-	float wall_time = 0;		//limit wall time of the program (s)
-	float extra_time = 0;		//wait extra time after time limit exceeded (s)
-	size_t stack_size = 0;		//limit stack size (kB), 0 = no limit
-	size_t files_size = 0;		//limit disk usage (kB), 0 = no limit
-	size_t disk_blocks = 0;		//disk quota in blocks
-	size_t disk_inodes = 0;		//disk quota in inodes
-	std::string std_input;		//redirect stdin from file
-	std::string std_output;		//redirect stdout to file
-	std::string std_error;		//redirect stderr to file
-	std::string chdir;		//change working directory inside sandbox
-	size_t processes = 0;		//limit number of processes, 0 = no limit
-	bool share_net = false;			//allow use host network
-	std::map<std::string, std::string> environ_vars; //set environment variables
+	/**
+	* Limit memory usage. For Isolate, this limits whole control group (--cg-mem switch).
+	* Memory size is set in kilobytes.
+	*/
+	size_t memory_usage;
+	/**
+	* Limit total run time by CPU time. For Isolate, this is for whole control group.
+	* Time is set in seconds and can be fractional.
+	*/
+	float cpu_time;
+	/**
+	* Limit total run time by wall clock. Time is set in seconds and can be fractional.
+	*/
+	float wall_time;
+	/**
+	* Set extra time before kill the process. If program finishes in this extra amount of
+	* time, it won't succeeded, but total run time will be reported to results log. This
+	* time is also in (fractional) seconds.
+	*/
+	float extra_time;
+	/**
+	* Limit stack size. This is additional memory limit, 0 is no special limit for stack,
+	* global memory rules will aply. Otherwise, max stack size is @a stack_size kilobytes.
+	*/
+	size_t stack_size;
+	/**
+	* Limit size of created files. This could be useful, if your filesystem doesn't support
+	* quotas. Otherwise, see items @a disk_blocks and @a disk_inodes. 0 means not set.
+	*/
+	size_t files_size;
+	/**
+	* Set disk quota to given number of blocks.
+	* @warning Underlying filesystem must support quotas.
+	*/
+	size_t disk_blocks;
+	/**
+	* Set disk quota to given number of inodes.
+	* @warning Underlying filesystem must support quotas.
+	*/
+	size_t disk_inodes;
+	/**
+	* Redirect standard input from given file.
+	* @note Path must be accessible from inside of sandbox.
+	*/
+	std::string stdin;
+	/**
+	* Redirect standard output to given file.
+	* @note Path must be accessible from inside of sandbox.
+	*/
+	std::string stdout;
+	/**
+	* Redirect standard error output to given file.
+	* @note Path must be accessible from inside of sandbox.
+	*/
+	std::string stderr;
+	/**
+	* Change working directory to subdirectory inside the sandbox.
+	* @note Path must be accessible from inside of sandbox.
+	*/
+	std::string chdir;
+	/**
+	* Limit number of processes/threads that could be created.
+	* 0 means no limit.
+	*/
+	size_t processes;
+	/**
+	* Allow to share host computers network. Otherwise, dedicated
+	* local interface will be created.
+	*/
+	bool share_net;
+	/**
+	* Set environment variables before run command inside the sandbox.
+	*/
+	std::map<std::string, std::string> environ_vars;
+
 
 	bool operator==(const sandbox_limits &second) const
 	{
 		if (memory_usage != second.memory_usage ||
-				cpu_time != second.cpu_time ||
-				wall_time != second.wall_time ||
-				extra_time != second.extra_time ||
-				stack_size != second.stack_size ||
-				files_size != second.files_size ||
-				disk_blocks != second.disk_blocks ||
-				disk_inodes != second.disk_inodes ||
-				std_input != second.std_input ||
-				std_output != second.std_output ||
-				std_error != second.std_error ||
-				chdir != second.chdir ||
-				processes != second.processes ||
-				share_net != second.share_net ||
-				environ_vars != second.environ_vars) {
+			cpu_time != second.cpu_time ||
+			wall_time != second.wall_time ||
+			extra_time != second.extra_time ||
+			stack_size != second.stack_size ||
+			files_size != second.files_size ||
+			disk_blocks != second.disk_blocks ||
+			disk_inodes != second.disk_inodes ||
+			std_input != second.std_input ||
+			std_output != second.std_output ||
+			std_error != second.std_error ||
+			chdir != second.chdir ||
+			processes != second.processes ||
+			share_net != second.share_net ||
+			environ_vars != second.environ_vars) {
 			return false;
 		}
 
@@ -53,5 +114,3 @@ public:
 		return !((*this) == second);
 	}
 };
-
-#endif //CODEX_WORKER_SANDBOX_LIMITS_H
