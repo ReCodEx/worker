0333d343f8ca1c2bb598d743d855fa8dae23fc94
Petr Stefan <ptr.stef@gmail.com>
2016-01-08 23:01:53 +0100

    Add doc to archivator class.

--- src/archives/archivator.h
+++ src/archives/archivator.h
@@ -15,10 +15,34 @@
 
 namespace fs = boost::filesystem;
 
-
+/**
+ * Class for creating and decompressing archives.
+ * On error, both methods throws @ref archive_exception.
+ */
 class archivator {
 public:
+	/**
+	 * This method will create new .zip archive containing recursively all files inside
+	 * @a dir directory. The archive will contain one root directory (named as whole archive
+	 * without extension). Example:
+	 * @code{.cpp}
+	 * archivator::compress("/home", "/tmp/homes.zip");
+	 * @endcode
+	 * Code above will compress @a /home directory to archive @a /tmp/homes.zip. The archive will
+	 * contain one root directory @a homes, where are placed copies of all files and subdirectories
+	 * in @a /home.
+	 * @param dir Directory to compress.
+	 * @param destination Name and path to the destination archive (should have suffix .zip).
+	 */
 	static void compress(const std::string &dir, const std::string &destination);
+	/**
+	 * This method will decompress archive @a filename into directory @a destination.
+	 * Supported formats are mainly zip, tar, tar.gz, tar.bz2, 7zip. For more info
+	 * see https://github.com/libarchive/libarchive/wiki.
+	 * @note In source archive, paths containing ".." are not allowed!
+	 * @param filename Archive to extract.
+	 * @param destination Directory, where will be extracted files stored.
+	 */
 	static void decompress(const std::string &filename, const std::string &destination);
 private:
 	static void copy_data(archive *ar, archive *aw);
