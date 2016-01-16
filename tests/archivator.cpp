d5b0ab4e2b008368f66c65678580034a104da673
Teyras <teyras@gmail.com>
2016-01-16 13:33:20 +0100

    add a failing archivator test

--- tests/archivator.cpp
+++ tests/archivator.cpp
@@ -76,3 +76,27 @@ TEST(Archivator, CompressNonexistingDir)
 	EXPECT_THROW(archivator::compress((fs::current_path().root_path() / "nonexisting_dir").string(),
 										 (fs::temp_directory_path() / "archive.zip").string()), archive_exception);
 }
+
+TEST(Archivator, CompressAbsolutePath)
+{
+	auto archive_path = fs::temp_directory_path() / "archive_test";
+	fs::create_directories(archive_path);
+	fs::path test_file_path = archive_path / "test_file.txt";
+	fs::path result_path = fs::temp_directory_path() / "archive.zip";
+	fs::path extracted_path = fs::temp_directory_path() / "archive";
+
+	{
+		std::ofstream test_file(test_file_path.string());
+		test_file << "1234567";
+	}
+
+	archivator::compress(archive_path.string(), result_path.string());
+	ASSERT_TRUE(fs::is_regular_file(result_path));
+
+	EXPECT_NO_THROW(archivator::decompress(result_path.string(), fs::temp_directory_path().string()));
+	ASSERT_TRUE(fs::is_regular_file(extracted_path / "test_file.txt"));
+	ASSERT_EQ((size_t) 7, fs::file_size(extracted_path / "test_file.txt"));
+
+	fs::remove_all(archive_path);
+	fs::remove_all(extracted_path);
+}
