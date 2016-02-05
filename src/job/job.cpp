a636248db9e9b73bdae579d8685a21fe87273320
Martin Polanka <PolankaMartin@gmail.com>
2016-02-05 21:22:29 +0100

    Conflicts in job.cpp resolved.

--- src/job/job.cpp
+++ src/job/job.cpp
@@ -2,14 +2,9 @@
 #include "job_exception.h"
 
 job::job(std::shared_ptr<job_metadata> job_meta, std::shared_ptr<worker_config> worker_conf,
-<<<<<<< HEAD
 		 fs::path source_path, fs::path result_path, std::shared_ptr<file_manager_base> fileman)
 	: job_meta_(job_meta), worker_config_(worker_conf), source_path_(source_path),
 	  result_path_(result_path), fileman_(fileman)
-=======
-		 fs::path source_path, std::shared_ptr<file_manager_base> fileman)
-	: job_meta_(job_meta), worker_config_(worker_conf), source_path_(source_path), fileman_(fileman)
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 {
 	// check construction parameters if they are in right format
 	if (job_meta_ == nullptr) {
@@ -26,12 +21,9 @@ job::job(std::shared_ptr<job_metadata> job_meta, std::shared_ptr<worker_config> 
 		throw job_exception("File manager cannot be null");
 	}
 
-<<<<<<< HEAD
 	// construct system logger for this job
 	init_logger();
 
-=======
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 	// build job from given job configuration
 	build_job();
 }
@@ -56,13 +48,8 @@ void job::build_job()
 	// create fake task, which is logical root of evaluation
 	size_t id = 0;
 	std::map<std::string, size_t> eff_indegree;
-<<<<<<< HEAD
 	root_task_ = std::make_shared<fake_task>(id++);
 	eff_indegree.insert(std::make_pair(root_task_->get_task_id(), 0));
-=======
-	auto root_task = std::make_shared<fake_task>(id++);
-	eff_indegree.insert(std::make_pair(root_task->get_task_id(), 0));
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 
 
 	// construct all tasks with their ids and check if they have all datas, but do not connect them
@@ -124,19 +111,11 @@ void job::build_job()
 			if (limits->processes == SIZE_MAX) {
 				limits->processes = worker_limits.processes;
 			}
-<<<<<<< HEAD
 			if (limits->disk_size == SIZE_MAX) {
 				limits->disk_size = worker_limits.disk_size;
 			}
 			if (limits->disk_files == SIZE_MAX) {
 				limits->disk_files = worker_limits.disk_files;
-=======
-			if (limits->disk_blocks == SIZE_MAX) {
-				limits->disk_blocks = worker_limits.disk_blocks;
-			}
-			if (limits->disk_inodes == SIZE_MAX) {
-				limits->disk_inodes = worker_limits.disk_inodes;
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 			}
 
 			// ... and finally construct external task from given information
@@ -146,11 +125,7 @@ void job::build_job()
 			std::shared_ptr<task_base> task = std::make_shared<external_task>(
 						worker_config_->get_worker_id(), id++, task_meta->task_id, task_meta->priority,
 						task_meta->fatal_failure, task_meta->dependencies, task_meta->binary,
-<<<<<<< HEAD
 						task_meta->cmd_args, sandbox->name, *limits, logger_);
-=======
-						task_meta->cmd_args, sandbox->name, *limits);
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 			unconnected_tasks.insert(std::make_pair(task_meta->task_id, task));
 			eff_indegree.insert(std::make_pair(task_meta->task_id, 0));
 
@@ -165,7 +140,6 @@ void job::build_job()
 			if (task_meta->binary == "cp") {
 				task = std::make_shared<cp_task>(id++, task_meta->task_id, task_meta->priority,
 					task_meta->fatal_failure, task_meta->binary,
-<<<<<<< HEAD
 					task_meta->cmd_args, task_meta->dependencies);
 			} else if (task_meta->binary == "mkdir") {
 				task = std::make_shared<mkdir_task>(id++, task_meta->task_id, task_meta->priority,
@@ -191,33 +165,6 @@ void job::build_job()
 				task = std::make_shared<fetch_task>(id++, task_meta->task_id, task_meta->priority,
 					task_meta->fatal_failure, task_meta->binary,
 					task_meta->cmd_args, task_meta->dependencies, fileman_);
-=======
-					task_meta->cmd_args, "", task_meta->dependencies);
-			} else if (task_meta->binary == "mkdir") {
-				task = std::make_shared<mkdir_task>(id++, task_meta->task_id, task_meta->priority,
-					task_meta->fatal_failure, task_meta->binary,
-					task_meta->cmd_args, "", task_meta->dependencies);
-			} else if (task_meta->binary == "rename") {
-				task = std::make_shared<rename_task>(id++, task_meta->task_id, task_meta->priority,
-					task_meta->fatal_failure, task_meta->binary,
-					task_meta->cmd_args, "", task_meta->dependencies);
-			} else if (task_meta->binary == "rm") {
-				task = std::make_shared<rm_task>(id++, task_meta->task_id, task_meta->priority,
-					task_meta->fatal_failure, task_meta->binary,
-					task_meta->cmd_args, "", task_meta->dependencies);
-			} else if (task_meta->binary == "archivate") {
-				task = std::make_shared<archivate_task>(id++, task_meta->task_id, task_meta->priority,
-					task_meta->fatal_failure, task_meta->binary,
-					task_meta->cmd_args, "", task_meta->dependencies);
-			} else if (task_meta->binary == "extract") {
-				task = std::make_shared<extract_task>(id++, task_meta->task_id, task_meta->priority,
-					task_meta->fatal_failure, task_meta->binary,
-					task_meta->cmd_args, "", task_meta->dependencies);
-			} else if (task_meta->binary == "fetch") {
-				task = std::make_shared<fetch_task>(id++, task_meta->task_id, task_meta->priority,
-					task_meta->fatal_failure, task_meta->binary,
-					task_meta->cmd_args, "", task_meta->dependencies, fileman_);
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 			} else {
 				throw job_exception("Unknown internal task: " + task_meta->binary);
 			}
@@ -234,13 +181,8 @@ void job::build_job()
 
 		// connect all suitable task underneath root
 		if (depend.size() == 0) {
-<<<<<<< HEAD
 			root_task_->add_children(elem.second);
 			elem.second->add_parent(root_task_);
-=======
-			root_task->add_children(elem.second);
-			elem.second->add_parent(root_task);
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 			eff_indegree.at(elem.first) = 1;
 		} else {
 			// write indegrees to every task
@@ -261,17 +203,12 @@ void job::build_job()
 
 	// all should be done now... just linear ordering is missing...
 	try {
-<<<<<<< HEAD
 		helpers::topological_sort(root_task_, eff_indegree, task_queue_);
-=======
-		helpers::topological_sort(root_task, eff_indegree, task_queue_);
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 	} catch (helpers::top_sort_exception &e) {
 		throw job_exception(e.what());
 	}
 }
 
-<<<<<<< HEAD
 std::vector<std::pair<std::string, std::shared_ptr<task_results>>> job::run()
 {
 	std::vector<std::pair<std::string, std::shared_ptr<task_results>>> results;
@@ -303,29 +240,10 @@ std::vector<std::pair<std::string, std::shared_ptr<task_results>>> job::run()
 			} else {
 				// set executable bit in this task and in children
 				logger_->info() << "Task failed - childs will not be executed";
-=======
-void job::run()
-{
-	// simply run all tasks in given topological order
-	for (auto &i : task_queue_) {
-		try {
-			if (i->is_executable()) {
-				i->run();
-			} else {
-				// we have to pass information about non-execution to children
-				i->set_children_execution(false);
-			}
-		} catch (std::exception) {
-			if (i->get_fatal_failure()) {
-				break;
-			} else {
-				// set executable bit in this task and in children
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 				i->set_execution(false);
 				i->set_children_execution(false);
 			}
 		}
-<<<<<<< HEAD
 		logger_->info() << "Task " << task_id << " finished";
 	}
 
@@ -352,29 +270,11 @@ void job::init_logger()
 		file_logger->info() << "       Job system log";
 		file_logger->info() << "------------------------------";
 		logger_ = file_logger;
-	} catch(spdlog::spdlog_ex &e) {
+	} catch (spdlog::spdlog_ex &e) {
 		//Suppose not happen. But in case, create only empty logger.
 		auto sink = std::make_shared<spdlog::sinks::null_sink_st>();
 		logger_ = std::make_shared<spdlog::logger>("job_manager_nolog", sink);
 	}
-=======
-	}
-
-	return;
-}
-
-std::map<std::string, std::shared_ptr<task_results>> job::get_results()
-{
-	std::map<std::string, std::shared_ptr<task_results>> res;
-
-	for (auto &i : task_queue_) {
-		if (i->get_result() != nullptr) {
-			res.emplace(i->get_task_id(), i->get_result());
-		}
-	}
-
-	return res;
->>>>>>> 95a65be80c92a8573532a61469cf3328660ffe6f
 }
 
 void job::cleanup_job()
