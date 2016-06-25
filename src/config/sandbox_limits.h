d888cd1d88697e7a7b0fa92d6365286a6806ea21
Martin Polanka <PolankaMartin@gmail.com>
2016-06-25 11:21:17 +0200

    Documentation of helper functions

--- src/config/sandbox_limits.h
+++ src/config/sandbox_limits.h
@@ -133,4 +133,4 @@ public:
 	}
 };
 
-#endif
+#endif // RECODEX_WORKER_SANDBOX_LIMITS_H
--- src/helpers/zmq_socket.h
+++ src/helpers/zmq_socket.h
@@ -7,10 +7,21 @@
 
 namespace helpers
 {
-
+	/**
+	 * Sends multipart message through given zmq socket.
+	 * @param socket socket to which messages will be sent
+	 * @param msg multipart message which will be sent
+	 * @return true on success, false otherwise.
+	 */
 	bool send_through_socket(zmq::socket_t &socket, const std::vector<std::string> &msg);
+	/**
+	 * Receive multipart message from given zmq socket.
+	 * @param socket messages should be received here
+	 * @param target reference to modifiable vector, after calling it should contain received messages
+	 * @param terminate pointer to boolean variable which will be set to true if exception occured on receiving
+	 * @return true on success, false otherwise
+	 */
 	bool recv_from_socket(zmq::socket_t &socket, std::vector<std::string> &target, bool *terminate = nullptr);
 }
-
 
 #endif // RECODEX_HELPERS_ZMQ_SOCKET_H
