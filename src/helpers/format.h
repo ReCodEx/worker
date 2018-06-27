ce739703a2208e86c107e1c31c3a9c54316a2fea
Petr Stefan <ptr.stef@gmail.com>
2018-06-27 21:52:51 +0200

    Attempt to fix Windows build (1)

--- src/helpers/format.h
+++ src/helpers/format.h
@@ -5,15 +5,23 @@
 
 namespace helpers
 {
+
+	/**
+	 * Last step of recursive template
+	 */
 	inline void format(std::ostringstream &)
 	{
 	}
 
+	/**
+	 * Concatenate various types into one string
+	 */
 	template <typename ArgT, typename... T> void format(std::ostringstream &oss, const ArgT &a, T... args)
 	{
 		oss << a;
 		format(oss, args...);
 	}
+
 } // namespace helpers
 
 #endif // RECODEX_WORKER_HELPERS_FORMAT_H
--- src/helpers/type_utils.h
+++ src/helpers/type_utils.h
@@ -1,6 +1,9 @@
 #ifndef RECODEX_WORKER_HELPERS_TYPE_UTILS_HPP
 #define RECODEX_WORKER_HELPERS_TYPE_UTILS_HPP
 
+// shitty Windows and their own shitty min() and max() macros
+#define NOMINMAX
+
 #include <cmath>
 #include <limits>
 #include <type_traits>
@@ -9,6 +12,13 @@
 namespace helpers
 {
 
+	/**
+	 * Floating point types comparison with some epsion.
+	 * @param x First number
+	 * @param y Second number
+	 * @param ulp Desired precision in ULPs (units in the last place)
+	 * @returns @a true if the numbers are almost the same
+	 */
 	template <class T>
 	typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type almost_equal(T x, T y, int ulp = 2)
 	{
