45f6b839084c1793a5092f9d035c482ea259fa80
Martin Krulis <krulis@ksi.mff.cuni.cz>
2020-12-04 17:14:59 +0100

    Updating passthrough judge and corresponding wrapper to handle both stdout and stderr.

--- judges/judge_passthrough/recodex-data-only-wrapper.sh
+++ judges/judge_passthrough/recodex-data-only-wrapper.sh
@@ -9,6 +9,15 @@ if [[ "$EXECUTABLE" == *.jar ]]; then
 	EXECUTABLE="java -jar $EXECUTABLE"
 fi
 
-OUTPUT=`"$EXECUTABLE" "$@"`
+TMPFILE=`mktemp`
+OUTPUT=`"$EXECUTABLE" "$@" 2>"$TMPFILE"`
+
 echo $?
-echo "$OUTPUT"
+
+echo "$OUTPUT" | base64 -w 0
+echo
+
+cat "$TMPFILE" | base64 -w 0
+echo
+
+rm -rf "$TMPFILE" 1>/dev/null 2>/dev/null
